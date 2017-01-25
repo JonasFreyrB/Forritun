171e4089bb14ce395037c7691b19707cc8d2455a
JonasFreyrB <jonasbj226@nemi.tskoli.is>
2017-01-25 14:18:03 +0000

    Dæmi 2 lokið

--- git_verkefni_forritun.py
+++ git_verkefni_forritun.py
@@ -3,3 +3,8 @@ tala1=int(input("Sláðu inn tölu 1 "))
 tala2=int(input("Sláðu inn tölu 2 "))
 print("Tölurnar lagðar saman ",tala1+tala2)
 print("Tölurnar margfaldaðar saman ",tala1*tala2)
+
+#Liður 2
+fornafn=input("Sláðu inn fornafnið þitt ")
+eftirnafn=input("Sláðu inn eftirnafnið þitt ")
+print("Halló",fornafn,eftirnafn) 
