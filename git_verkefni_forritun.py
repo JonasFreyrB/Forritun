ff5568ee36ac501955825dfdb6903d50285c8ca3
JonasFreyrB <jonasbj226@nemi.tskoli.is>
2017-01-25 14:14:46 +0000

    Dæmi eitt lokið

---
+++ git_verkefni_forritun.py
@@ -0,0 +1,5 @@
+#Liður 1
+tala1=int(input("Sláðu inn tölu 1 "))
+tala2=int(input("Sláðu inn tölu 2 "))
+print("Tölurnar lagðar saman ",tala1+tala2)
+print("Tölurnar margfaldaðar saman ",tala1*tala2)
